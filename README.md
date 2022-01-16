67e2130f983a306e0a0e23b24d2094bd7cb6ddb4
Marcio Martinez <marcioedumartinez@gmail.com>
2022-01-15 22:19:54 -0300

    feat: core functions added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rsa-with-python
 A simple program for cypher and decypher in RSA cryptosystem
+
+### example private keys:
+P = 17
+Q = 23
+D = 235
+
+### exaple public keys:
+N = 391
+E = 3
---
+++ RSA.py
@@ -0,0 +1,33 @@
+class RSA():
+    def __init__(self):
+
+        # private keys:
+        self.p = 0
+        self.q = 0
+        self.d = 0
+
+        # public keys:
+        self.n = self.p * self.q
+        self.e = 0
+
+    def __message_to_array(self, message):
+        return list(map(ord, message))
+
+    def __iterable_to_message(self, iterable):
+        return "".join(map(chr, iterable))
+
+    def __cypher_unicode_char(self, char):
+        return char ** self.e % self.n
+
+    def __decypher_unicode_char(self, char):
+        return char ** self.d % self.n
+
+    def cypher(self, message):
+        message_arr = self.__message_to_array(message)
+        encrypted_map_object = map(self.__cypher_unicode_char, message_arr)
+
+        return list(encrypted_map_object)
+
+    def decypher(self, message_array):
+        decrypted_message = map(self.__decypher_unicode_char, message_array)
+        return self.__iterable_to_message(decrypted_message)
